86021d9e8f1da5d8ef6f6378b44271b75625c548
MonikaTammineni <2300032536@kluniversity.in>
2025-07-30 10:17:21 +0530

    reeact project v1.0

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './style.css';
+
+function App(){
+    return (
+        <div className="container">
+            <h1>Welcome to My React Project - s105</h1>
+            <p>This is a simple React homepage deploed on github pages using vite</p>
+        </div>
+    );
+}
+export default App;
