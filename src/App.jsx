30be710be9dbd2396d72d00f87e00331535ef369
MonikaTammineni <2300032536@kluniversity.in>
2025-07-31 12:36:10 +0530

    update app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ function App(){
             <h1>Welcome to My React Project - s105</h1>
             <h1>CI/CD and Cloud DevOps</h1>
             <p>This is a simple React homepage deploed on GitHub Actions using vite</p>
+            <h1>done with Git pages and Git actions</h1>
         </div>
     );
 }
